ec34f034285352c14dda1dc4737724e012b3c53e
KunnuSherry <gamingvenom465@gmail.com>
2025-02-10 07:17:57 +0530

    Started Learning Dart - Basics

---
+++ Dart/02_operators.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(3+2*2); // 7
+  //print('3'+2); error
+  print('3'+'2'); //32
+  print('3'*2); //33
+  //DART FOLLOWS BODMAS
+  /* Multiple
+   line
+   comment
+  */
+}
---
+++ Dart/03_variables.dart
@@ -0,0 +1,28 @@
+void main() {
+  //variables
+  
+  //For Integers
+  int a = 10;
+  int b = 20;
+  print(a+b);
+  print(a);
+  
+  //For Decimal Numbers
+  double c = 10.10;
+  double d = 11.22;
+  print(c*d);
+  print(c);
+  
+  //For words, chars
+  String firstValue = "H";
+  print(firstValue);
+  
+  //Booleans
+  bool f = false;
+  print(f);
+  
+  //Dynamic
+  dynamic cd = 10;
+  print(cd);
+  
+}
